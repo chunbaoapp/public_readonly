352426226aaf8af2f656b081d2f03f250b6d215f
AMS\Yang_C <chunbao.yang@schiphol.nl>
2021-08-02 19:44:31 +0200

    Add h2 support

--- src/main/java/chunbao/nl/event/readonly/ReadonlyApplication.java
+++ src/main/java/chunbao/nl/event/readonly/ReadonlyApplication.java
@@ -2,12 +2,13 @@ package chunbao.nl.event.readonly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("chunbao.nl.event.module") // scan h2 entity
 public class ReadonlyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReadonlyApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ReadonlyApplication.class, args);
+  }
 }
--- src/main/java/chunbao/nl/event/readonly/controller/UserController.java
+++ src/main/java/chunbao/nl/event/readonly/controller/UserController.java
@@ -1,6 +1,8 @@
 package chunbao.nl.event.readonly.controller;
 
 import chunbao.nl.event.module.GuiUser;
+import chunbao.nl.event.readonly.dao.GuiUserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("user-rest")
 public class UserController {
+
+  @Autowired GuiUserRepo guiUserRepo;
+
   @GetMapping(value = "/{username}", produces = "application/json")
   public GuiUser getUser(@PathVariable String username) {
     GuiUser myUser = new GuiUser();
     myUser.setUsername(username);
     myUser.setPassword("PW:" + username);
+    guiUserRepo.save(myUser);
     return myUser;
   }
 }
